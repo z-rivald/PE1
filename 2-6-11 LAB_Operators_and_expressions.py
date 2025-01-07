8c37a38e2e6a69714e6ad3de319f1961379f1563
z-rivald <127426656+z-rivald@users.noreply.github.com>
2025-01-07 09:12:38 +0500

    Update 2-6-11 LAB_Operators_and_expressions.py

--- 2-6-11 LAB_Operators_and_expressions.py
+++ 2-6-11 LAB_Operators_and_expressions.py
@@ -2,6 +2,4 @@ hour = int(input("Starting time (hours): "))
 mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
 
-# Write your code here.
-
 print(str((hour*60+mins+dura)//60%24)+":"+str((hour*60+mins+dura)%60))
